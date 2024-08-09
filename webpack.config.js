1a5b0e4cb88b575bdd96addf6472eff8cf09ae96
boconnor1993 <brendan_oconnor1993@hotmail.com>
2024-08-09 23:35:35 +1000

    Added initial files for todo project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: './', // Updated to handle relative paths
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    hot: true,
+    open: true,
+  },
+};
